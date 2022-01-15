ebea7232c01122771e5f04a657e70d7c6d53ad8e
Tony <tonycarpenter@gmail.com>
2022-01-15 16:59:36 -0500

    Finish rough layout of comp and minor functionaltiy

---
+++ src/classes/customers.js
@@ -0,0 +1,11 @@
+class Customers {
+    constructor(customer) {
+      this.id = customer.id;
+      this.name = customer.name;
+    }
+    calculateTotalSpent() {
+      console.log("hey");
+    }
+  }
+
+export default Customers;
--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,19 @@
 import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
+import Customers from './classes/Customers.js';
+import Bookings from './classes/Bookings.js';
+import Rooms from './classes/Rooms.js';
 
 ////// eventually break down the following into it's own file:
 // import {customersAPI, roomsAPI, bookingsAPI,} from './apiCalls';
 
 const customersAPI = fetch('http://localhost:3001/api/v1/customers').then(response => response.json()).catch(error => console.log(error));
-//GET all customers. Sample response: object with customers property containing an array of all customers
 //GET single customer - http://localhost:3001/api/v1/customers/<id> where <id> will be a number of a customer's id. Sample response: object of single customer's info
 
 const roomsAPI = fetch('http://localhost:3001/api/v1/rooms').then(response => response.json()).catch(error => console.log(error));
-//GET all rooms. Sample response: object with rooms property containing an array of all rooms
 
 const bookingsAPI = fetch('http://localhost:3001/api/v1/bookings').then(response => response.json()).catch(error => console.log(error));
-//GET all bookings. Sample response: object with bookings property containing an array of all bookings
 //POST - Add new booking - required properties	{ "userID": 48, "date": "2019/09/23", "roomNumber": 4 }. Sample response: { message: 'Booking with id <id> successfully posted', newBooking: <Object with trip info just posted> }
 //DELETE - Delete single booking http://localhost:3001/api/v1/bookings/<id> where <id> will be a number of a booking's id. Sample response:	{ message: Booking #<id> has been deleted }
 
@@ -38,15 +38,18 @@ export {
 let customers;
 let rooms;
 let bookings;
+let user;
 
 Promise.all([customersAPI, roomsAPI, bookingsAPI]).then(data => {
-    customers = data[0];
-    rooms = data[1];
-    bookings = data[2];
-    // console.log('customers: ', customers)
-    console.log('rooms :', rooms)
-    console.log('bookings: ', bookings)
-  }).catch(error => console.log(error));
+    customers = data[0].customers.map(customer => new Customers(customer))
+        // console.log('customers: ', customers)
+    user = customers[getRandomIndex(customers)];
+        // console.log("user: ", user)
+    rooms = data[1].rooms.map(room => new Rooms(room))
+        // console.log('rooms :', rooms)
+    bookings = data[2].bookings.map(booking => new Bookings(booking))
+        console.log('bookings: ', bookings)
+}).catch(error => console.log(error));
 
 
 
@@ -62,21 +65,27 @@ const viewHome = document.getElementById('viewHome');
 const viewCustomerDashboard = document.getElementById('viewCustomerDashboard');
 const viewNewBooking = document.getElementById('viewNewBooking');
 const calendar = document.getElementById('calendar');
+const resultsFilter = document.getElementById('resultsFilter');
+const listingsHeaderMessage = document.getElementById('listingsHeaderMessage');
 const availableRoomsToBook = document.getElementById('availableRoomsToBook');
 
+const getRandomIndex = (arr) => {
+    return Math.floor(Math.random() * arr.length);
+};
+
 const showViewHome = () => {
-    hide([viewCustomerDashboard, viewNewBooking]);
+    hide([viewCustomerDashboard, viewNewBooking, resultsFilter]);
     show([viewHome]);
 };
 
 const showViewCustomerDashboard = () => {
-    hide([viewHome, viewNewBooking]);
+    hide([viewHome, viewNewBooking, resultsFilter]);
     show([viewCustomerDashboard]);
     showBookings();
 };
 
 const showViewNewBooking = () => {
-    hide([viewCustomerDashboard, viewHome]);
+    hide([viewCustomerDashboard, viewHome, resultsFilter]);
     show([viewNewBooking]);
 };
 
@@ -89,26 +98,37 @@ const show = (array) => {
 };
 
 const loginUser = () => {
-    console.log("user is logged in")
+    console.log("user is logged in");
     //form may refresh page?
 }
 
 const bookStay = () => {
-    console.log("stay has been booked")
+    console.log("stay has been booked");
 }
 
 const showResult = (item) => {
     return `Date: ${item.date}, Room Number: ${item.roomNumber}<br/>`
 }
 
-const showRoom = (item) => {
-    return `Room Number: ${item.number}<br/>
-    Room Type: ${item.roomTypebedSize}<br/>
-    Bed Size: ${item.bedSize}<br/>
+const uppercaseFirstLetter = (item) => {
+    let phrase = item.split(" ").map((letter) => letter.charAt(0).toUpperCase() + letter.substring(1)).join(' ');
+    return phrase;
+}
+
+
+const showRoom = (item, event) => {
+    return `
+    <section class="available-rooms-to-book">
+    Room Number: ${item.number}<br/>
+    Type: ${uppercaseFirstLetter(item.roomType)}<br/>
+    Bed Size: ${uppercaseFirstLetter(item.bedSize)}<br/>
     Number of Beds: ${item.numBeds}<br/>
-    Cost Per Night: ${item.costPerNight}<br/>
-    Bidet: ${item.bidet}<br/>
-    <br/>`
+    Cost Per Night: $${item.costPerNight.toFixed(2)}<br/>
+    Bidet: ${item.bidet ? "Yes" : "No"}<br/>
+    <br/>
+    <center><button data-user-id="${user.id}" data-picked-date="${event.target.value}" data-picked-room-number="${item.number}" class="nav-button" buttonBookStay>Book Room</button><br/></center>
+    <br/>
+    </section>`
 }
 
 const newBookingsAPI = () => {
@@ -117,35 +137,42 @@ const newBookingsAPI = () => {
 
 const showBookings = () => {
     newBookingsAPI().then(response => {
-        const userBookings = document.getElementById('userBookings')
-        const totalSpent = document.getElementById('totalSpent')
-        totalSpent.innerHTML = ''
-        userBookings.innerHTML = ''
-        let result = bookings.bookings.filter(booking => booking.userID === 1)
-        console.log(result)
+        const userBookings = document.getElementById('userBookings');
+        const totalSpent = document.getElementById('totalSpent');
+        totalSpent.innerHTML = '';
+        userBookings.innerHTML = '';
+        let result = bookings.bookings.filter(booking => booking.userID === user.id);
+        console.log(result);
         result.forEach(item => {
-            userBookings.innerHTML += showResult(item)
+            userBookings.innerHTML += showResult(item);
         })
         const totalCost = result.reduce((acc, item) =>{
-            let matchingRoom = rooms.rooms.find(room => item.roomNumber === room.number) 
-            acc += matchingRoom.costPerNight
+            let matchingRoom = rooms.rooms.find(room => item.roomNumber === room.number) ;
+            acc += matchingRoom.costPerNight;
             return acc;
         }, 0)
         totalSpent.innerHTML = `Total Spent: $${totalCost.toFixed(2)}`
-        console.log(totalCost.toFixed(2))
     }).catch(error => console.log(error));
 }
 
 const selectDate = (event) => {
-    let result = rooms.rooms.filter(room => {
-        let takenRooms = bookings.bookings.filter(booking => booking.roomNumber === room.number)
+    let date = event.target.value.replace('-', '/').replace('-', '/')
+    let result = rooms.filter(room => {
+        let takenRooms = bookings.filter(booking => booking.roomNumber === room.number);
         return !takenRooms.find(room => {
-            return room.date === event.target.value
+            return room.date === date;
         })
     })
-    result.forEach(room => {
-        availableRoomsToBook.innerHTML += showRoom(room)
-    })
+    if (result.length === 0) {
+        listingsHeaderMessage.innerHTML = `I'm sorry, but there are no rooms available for that date. Please search another date.`
+
+    } else {
+        result.forEach(room => {
+            show([resultsFilter]);
+            listingsHeaderMessage.innerHTML = `Available Rooms To Book For The Selected Day:<br/>`
+            availableRoomsToBook.innerHTML += showRoom(room, event);
+        })
+    }
 }
 
 buttonHome.addEventListener('click', showViewHome);
