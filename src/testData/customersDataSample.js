9e4622f647b118fff9207bb5133b40c229a48cf1
Tony <tonycarpenter@gmail.com>
2022-01-17 18:51:00 -0500

    Complete testing

---
+++ src/testData/customersDataSample.js
@@ -0,0 +1,10 @@
+module.exports = [
+  {
+    "id": 1,
+    "name": "Leatha Ullrich"
+  },
+  {
+    "id": 2,
+    "name": "Rocio Schuster"
+  }
+]
---
+++ src/testData/roomsDataSample.js
@@ -0,0 +1,18 @@
+module.exports = [
+  {
+    "number": 1,
+    "roomType": "residential suite",
+    "bidet": true,
+    "bedSize": "queen",
+    "numBeds": 1,
+    "costPerNight": 358.4
+  },
+  {
+    "number": 2,
+    "roomType": "suite",
+    "bidet": false,
+    "bedSize": "full",
+    "numBeds": 2,
+    "costPerNight": 477.38
+  }
+]
